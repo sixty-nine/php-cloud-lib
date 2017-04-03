b97227e4751763f472a93aed06c1e279b203b815
Daniel Barsotti <email>
2017-04-03 08:25:57 +0200

    Add more filters tests

--- src/SixtyNine/Cloud/Tests/Filters/FiltersTest.php
+++ src/SixtyNine/Cloud/Tests/Filters/FiltersTest.php
@@ -6,7 +6,9 @@ namespace SixtyNine\Cloud\Tests\Filters;
 
 use SixtyNine\Cloud\Filters\FilterInterface;
 use SixtyNine\Cloud\Filters\Filters;
+use SixtyNine\Cloud\Filters\RemoveCharacters;
 use SixtyNine\Cloud\Filters\RemoveNumbers;
+use SixtyNine\Cloud\Filters\RemoveTrailingCharacters;
 use SixtyNine\Cloud\Filters\RemoveWords;
 use SixtyNine\Cloud\Model\Word;
 
@@ -27,28 +29,31 @@ class FiltersTest extends \PHPUnit_Framework_TestCase
     public function wordsProvider()
     {
         return array(
-            [
-                new RemoveWords(array('and')),
-                'foobar',
-                true
-            ],
-            [
-                new RemoveWords(array('and')),
-                'and',
-                false
-            ],
-            [
-                new RemoveNumbers(),
-                '1234abcd5678',
-                true,
-                'abcd'
-            ],
-            [
-                new RemoveNumbers(),
-                '1234567890',
-                true,
-                ''
-            ],
+            [new RemoveWords(array('and')), 'foobar', true],
+            [new RemoveWords(array('and')), 'and', false],
+            [new RemoveNumbers(), '1234abcd5678', true, 'abcd'],
+            [new RemoveNumbers(), '1234567890', true, ''],
+            [new RemoveTrailingCharacters(), '1234', true, '1234'],
+            [new RemoveTrailingCharacters(), '12345.', true, '12345'],
+            [new RemoveTrailingCharacters(), '12345.,', true, '12345'],
+            [new RemoveTrailingCharacters(), '12345!?', true, '12345'],
+            [new RemoveTrailingCharacters(), '1234', true, '1234'],
+            [new RemoveWords(), 'and', false],
+            [new RemoveWords(), 'our', false],
+            [new RemoveWords(), 'yours', false],
+            [new RemoveWords(), 'such', false],
+            [new RemoveWords(), 'some', true, 'some'],
+            [new RemoveWords(), 'other', true, 'other'],
+            [new RemoveWords(), 'words', true, 'words'],
+            [new RemoveCharacters(), '12345', true, '12345'],
+            [new RemoveCharacters(), '123?4', true, '1234'],
+            [new RemoveCharacters(), '12345"', true, '12345'],
+            [new RemoveCharacters(), '\'12345\'', true, '12345'],
+            [new RemoveCharacters(), '?!1234!?', true, '1234'],
+            [new RemoveTrailingCharacters(), '1234', true, '1234'],
+            [new RemoveTrailingCharacters(), '12345.', true, '12345'],
+            [new RemoveTrailingCharacters(), '12345,.', true, '12345'],
+            [new RemoveTrailingCharacters(), '12345!?', true, '12345'],
         );
     }
 
